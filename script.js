5a116b146ecc2b34fc40316e87d0faaede3d9e5f
pappusrivastav <144468141+pappusrivastav@users.noreply.github.com>
2023-11-02 14:39:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function decimalToBinary(num) {
-  //Write you code here
+// function decimalToBinary(num) {
+//   //Write you code here
   
+// }
+
+// window.decimalToBinary = decimalToBinary;
+function decimalToBinary(num) {
+    return num.toString(2);
 }
 
 window.decimalToBinary = decimalToBinary;
